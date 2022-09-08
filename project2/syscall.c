4e8cd98d30fa9751162898a11b9c51f7a168bb86
Youngmin Park <ympark1230@naver.com>
2022-09-08 21:37:39 +0900

    trace 구현 중

--- project2/syscall.c
+++ project2/syscall.c
@@ -7,6 +7,8 @@
 #include "x86.h"
 #include "syscall.h"
 
+#define T_TRACE 1
+
 // User code makes a system call with INT T_SYSCALL.
 // System call number in %eax.
 // Arguments on the stack, from the user call to the C
@@ -104,6 +106,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_memsize(void);
+extern int sys_trace(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -128,6 +131,33 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_memsize] sys_memsize,
+[SYS_trace] sys_trace,
+};
+
+static char *syscall_names[] = {
+  [SYS_fork]    "fork",
+  [SYS_exit]    "exit",
+  [SYS_wait]    "wait",
+  [SYS_pipe]    "pipe",
+  [SYS_read]    "read",
+  [SYS_kill]    "kill",
+  [SYS_exec]    "exec",
+  [SYS_fstat]   "fstat",
+  [SYS_chdir]   "chdir",
+  [SYS_dup]     "dup",
+  [SYS_getpid]  "getpid",
+  [SYS_sbrk]    "sbrk",
+  [SYS_sleep]   "sleep",
+  [SYS_uptime]  "uptime",
+  [SYS_open]    "open",
+  [SYS_write]   "write",
+  [SYS_mknod]   "mknod",
+  [SYS_unlink]  "unlink",
+  [SYS_link]    "link",
+  [SYS_mkdir]   "mkdir",
+  [SYS_close]   "close",
+  [SYS_memsize] "memsize",
+  [SYS_trace] "trace",
 };
 
 void
@@ -136,9 +166,21 @@ syscall(void)
   int num;
   struct proc *curproc = myproc();
 
+  int is_traced = (curproc->traced & T_TRACE);
+
   num = curproc->tf->eax;
+
+  // if(num == SYS_exit && is_traced){
+  //   cprintf("syscall traced: pid = %d, syscall = %s, %d returned",
+  //   curproc->pid, syscall_names[num], curproc->tf);
+  // }
+
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
+    if(is_traced){
+      cprintf("syscall traced: pid = %d, syscall = %s, %d returned",
+      curproc->pid, syscall_names[num], curproc->tf->eax);
+    }
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
