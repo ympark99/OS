be98042d72d1dce61ba6fa860d575c9994622faa
Youngmin Park <ympark1230@naver.com>
2022-09-17 12:31:29 +0900

    proc 주석 추가

--- project2/proc.h
+++ project2/proc.h
@@ -49,7 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int traced;
+  int traced; // 시스템 콜 중 관련 정보 호출
 };
 
 // Process memory is laid out contiguously, low addresses first:
