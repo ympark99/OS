4ecb99e70234afee84ae939a1b049ffdb74cb3cf
Youngmin Park <ympark1230@naver.com>
2022-10-06 19:02:14 +0900

    sdebug 구현 init

---
+++ project3/sdebug.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define PNUM 5
+#define PRINT_CYCLE 100000000
+#define TOTAL_COUNTER 500000000
+
+void sdebug_func(void)
+{
+    return;
+}
+
+int main(void)
+{
+    sdebug_func();
+    exit();
+}
