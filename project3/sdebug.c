21bc8e4acf5d69510a351db49a6823ce7439fbe8
Youngmin Park <ympark1230@naver.com>
2022-10-06 23:24:39 +0900

    sdebug 구현 중

--- project3/sdebug.c
+++ project3/sdebug.c
@@ -2,12 +2,29 @@
 #include "stat.h"
 #include "user.h"
 
-#define PNUM 5
-#define PRINT_CYCLE 100000000
-#define TOTAL_COUNTER 500000000
+#define PNUM 5 // 프로세스 개수
+#define PRINT_CYCLE 100000000 // 정보 출력 주기
+#define TOTAL_COUNTER 500000000 // 종료 시 counter 값
 
 void sdebug_func(void)
 {
+    int n, pid;
+
+    printf(1, "start sdebug command\n");
+
+    int pid = fork(); // 프로세스 생성
+    if(pid < 0){
+        printf(1, "fork error\n");
+        exit();
+    }
+    else if(pid == 0){ // 자식 코드
+        
+        exit();
+    }
+    else
+        wait(); // 대기    
+
+    printf(1, "end of sdebug command\n");
     return;
 }
 
