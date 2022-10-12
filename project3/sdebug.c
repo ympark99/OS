a4914c04d5d90f990d5d33f82da1ed8dee5d354a
Youngmin Park <ympark1230@naver.com>
2022-10-12 12:49:42 +0900

    sdebug 구현 및 테스트 완료

--- project3/sdebug.c
+++ project3/sdebug.c
@@ -8,26 +8,20 @@
 
 void sdebug_func(void)
 {
-    // int n;
-    int pid;
-    int i;
-    int counter = TOTAL_COUNTER;
-
-    int starttime, endtime;
+    int pid, i, starttime, endtime, counter;
+    
+    counter = TOTAL_COUNTER;
     starttime = uptime();
     printf(1, "start sdebug command\n");
 
     for(i = 0; i < PNUM; i++){
-        if((pid = fork()) == 0)
+        if((pid = fork()) == 0) // 프로세스 생성
             break;
+        else if(pid < 0){
+            printf(1, "fork error\n");
+        }
     }
 
-    // for(i = 0; i < PNUM; i++){
-        // pid = fork(); // 프로세스 생성
-        // if(pid < 0){
-        //     printf(1, "fork error\n");
-        //     exit();
-        // }
     if(pid == 0){ // 자식 코드
         weightset(i+1); // weight 값 1씩 증가하면서 부여
         pid = getpid();
@@ -47,49 +41,8 @@ void sdebug_func(void)
         }
         printf(1, "end of sdebug command\n");
     }
-    // }
     return;
 }
-
-// void sdebug_func(void)
-// {
-//     // int n;
-//     int pid;
-//     int i;
-
-//     printf(1, "start sdebug command\n");
-
-//     int starttime = uptime();
-//     for(i = 0; i < PNUM; i++){
-//         pid = fork(); // 프로세스 생성
-//         if(pid < 0){
-//             printf(1, "fork error\n");
-//             exit();
-//         }
-//         else if(pid == 0){ // 자식 코드
-//             // TOTAL_COUNTER만큼 반복
-//             for(uint counter = 0; counter < TOTAL_COUNTER; counter++){
-//                 if(counter == PRINT_CYCLE){                    
-//                     // TOTAL_COUNTER 프로세스 종료시 카운터 값
-//                     int endtime = uptime();
-//                     weightset(i+1); // weight 값 1씩 증가하면서 부여
-//                     // todo:프로세스 정보 출력
-//                     printf(1, "PID: %d, WEIGHT: %d, TIMES: %d ms\n", getpid(), i+1, (endtime-starttime) * 10);
-//                 }
-//             }
-//             printf(1, "PID: %d terminated\n", getpid());
-//             exit();
-//         }
-//     }
-//     for(; i > 0; i--){
-//         if(wait() < 0){
-//             printf(1, "wait stopped early\n");
-//             exit();
-//         }
-//     }
-//     printf(1, "end of sdebug command\n");
-//     return;
-// }
 
 int main(void)
 {
