ae570ded724cacc5a9050020e886fd1d2d69d419
Youngmin Park <ympark1230@naver.com>
2022-10-17 11:28:28 +0900

    최종 코드

--- project3/proc.h
+++ project3/proc.h
@@ -49,7 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int traced; // 시스템 콜 중 관련 정보 호출
+//   int traced; // 시스템 콜 중 관련 정보 호출
   int weight; // 가중치
   int priority; // 우선순위
 };
