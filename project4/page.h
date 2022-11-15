e14c69536aa45f113d3a098ad1f4fbb1f8b8314e
Youngmin Park <ympark1230@naver.com>
2022-11-15 18:01:39 +0900

    LFU 구현

--- project4/page.h
+++ project4/page.h
@@ -5,7 +5,15 @@
 #define BUF_SIZE 1024
 #define RAND_MIN 500
 
-int findLRU(int time[], int n);
 void start();
+void lfu_print(int frame_cnt, int frames[]);
+void lfu();
+int findLRU(int time[], int n);
+void lru();
+void lifo();
+void optimal();
+void fifo();
+void start();
+
 
 #endif
