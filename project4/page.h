2091c248b2864f7aa6b3ed03878f2a7c9902e05e
Youngmin Park <ympark1230@naver.com>
2022-11-17 12:50:34 +0900

    파일 저장 기능 구현

--- project4/page.h
+++ project4/page.h
@@ -5,9 +5,10 @@
 #define BUF_SIZE 1024
 #define RAND_MIN 500
 
+void esc();
 void sc();
 void start();
-void lfu_print(int frame_cnt, int frames[]);
+void lfu_print(int frame_cnt, int frames[], int flag, FILE *fp);
 void lfu();
 int findLRU(int time[], int n);
 void lru();
